a8d60e4231d8b011fe4987bcd724a4e688e9d6d3
luiserh1 <54946908+luiserh1@users.noreply.github.com>
2020-03-20 19:51:00 +0100

    Friday the 20th, March
    Animations, motion and clicking

---
+++ AVD/Assets/LuisSerranoHernandez/LaberinthPath/Scripts/SimpleGo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class SimpleGo : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform Destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = Destination.transform.position;
+    }
+}
